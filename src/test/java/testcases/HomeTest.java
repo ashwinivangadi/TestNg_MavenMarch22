cb7e2201743d211b682fe8a3228d599b7fa43361
ashwinivangadi <ashwinivangadi@gmail.com>
2022-03-19 05:13:08 +0000

    checkout

---
+++ src/test/java/testcases/HomeTest.java
@@ -0,0 +1,17 @@
+package testcases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class HomeTest {
+
+	@Test
+	public static void test1() {
+		
+		System.out.println("Inside test1");
+
+	}
+
+}
